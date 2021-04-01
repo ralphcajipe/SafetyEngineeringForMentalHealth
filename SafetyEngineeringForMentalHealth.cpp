41e791fef8d733933d08e649a721f99ac5641137
Ralph Cajipe <50406636+ralphcajipe@users.noreply.github.com>
2021-04-02 01:55:29 +0800

    Add experiment code

---
+++ SafetyEngineeringForMentalHealth.cpp
@@ -0,0 +1,22 @@
+entryCode = lock.getEntryCode();
+if (entryCode == lock.authorizedCode)
+{
+	  happyStatus = Happy.getStatus();
+	  sadLevel = SadSensor.get();
+	  if (sadLevel < dangerLevel)
+	  	state = safe;
+	  else
+	  	state = unsafe:
+	  if (happyStatus == Happy.inPlace() )
+	  	state = safe;
+	  if (state == safe)
+	  {
+	  	Door.locked = false;
+	  	Door.unlock();
+    }
+  	else
+    {
+      Door.lock();
+      Door.locked := true;
+    }
+}
