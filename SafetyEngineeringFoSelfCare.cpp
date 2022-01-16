452c67966c37ae56b51313ca9a1b8e20521d91f6
Ralph Cajipe <50406636+ralphcajipe@users.noreply.github.com>
2022-01-16 16:31:14 +0800

    Update SafetyEngineeringFoSelfCare.cpp

--- SafetyEngineeringFoSelfCare.cpp
+++ SafetyEngineeringFoSelfCare.cpp
@@ -1,4 +1,5 @@
 // a prototype: When I just want to be alone for awhile
+// surrounded by intelligent systems that can be trusted, e.g. jarvis
 entryCode = lock.getEntryCode();
 if (entryCode == lock.authorizedCode)
 {
